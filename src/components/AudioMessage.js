0cb6cb347c8d8b6f3cffd8f2e321e0d4e02def38
Dimasta <dimas1969592@gmail.com>
2022-01-02 22:48:45 +0200

    Finish audio paly!

--- src/components/AudioMessage.js
+++ src/components/AudioMessage.js
@@ -1,22 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import {colorFont} from '../constants/style';
+import {colorFont, errorColor} from '../constants/style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Sound from 'react-native-sound';
+import {Alert} from 'react-native';
+import {ERROR_AUDIO} from '../constants/error';
 
 const MessageAudio = ({name}) => {
+  const [playing, setPlayiyng] = useState(false);
+  const [sd, setSound] = useState(null);
+  const [width, setWidth] = useState(0);
+  const [pause, setPause] = useState(false);
+  const [interval, setInt] = useState(null);
+
+  const onSoundPlay = () => {
+    if (playing) {
+      if (sd.isPlaying()) {
+        setPause(true);
+        sd.pause();
+      } else {
+        setPause(false);
+
+        sd.play(s => {
+          if (s) {
+            setWidth(0);
+            setPlayiyng(false);
+            setPause(false);
+            clearInterval(interval);
+          }
+        });
+      }
+    } else {
+      let sound = new Sound(name.uri, Sound.MAIN_BUNDLE, err => {
+        if (err) {
+          Alert.alert('Помилка відтворення!', ERROR_AUDIO, [
+            {
+              text: 'Зрозуміло',
+            },
+          ]);
+        }
+
+        setSound(sound);
+        setPlayiyng(true);
+
+        let playInterval = setInterval(() => {
+          sound.getCurrentTime(currentTime =>
+            setWidth((currentTime / sound.getDuration()) * 100),
+          );
+          console.log('INTERVAL');
+        }, 1000);
+
+        setInt(playInterval);
+
+        sound.play(success => {
+          if (success) {
+            clearInterval(playInterval);
+            setPlayiyng(false);
+            setWidth(0);
+          }
+        });
+        sound.setVolume(1);
+      });
+    }
+  };
+
   return (
-    <MAW>
-      <MAC>
-        <IC>
-          <Icon name="audiotrack" color={'white'} size={25} />
-        </IC>
-        <AN numberOfLines={1} ellipsizeMode="tail">
-          {name.uri.split('/').pop()}
-        </AN>
-      </MAC>
+    <MAW onPress={onSoundPlay}>
+      <MACW>
+        <EC width={width}></EC>
+        <MAC>
+          <IC playing={playing}>
+            {playing && (
+              <>
+                {pause && <Icon name="pause" color={'white'} size={35} />}
+                {!pause && <Icon name="play-arrow" color={'white'} size={35} />}
+              </>
+            )}
+            {!playing && <Icon name="audiotrack" color={'white'} size={25} />}
+          </IC>
+          <AN numberOfLines={1} ellipsizeMode="tail">
+            {name.uri.split('/').pop()}
+          </AN>
+        </MAC>
+      </MACW>
     </MAW>
   );
 };
+
+const MACW = styled.View`
+  overflow: hidden;
+  width: 200px;
+`;
+
+const EC = styled.View`
+  width: ${({width}) => width}%;
+  height: 100%;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 4;
+  border-radius: 30px;
+`;
 
 const AN = styled.Text`
   font-size: 18px;
@@ -29,11 +112,12 @@ const IC = styled.View`
   width: 50px;
   height: 50px;
   border-radius: 100px;
-  background-color: ${colorFont};
+  background-color: ${({playing}) => (playing ? errorColor : colorFont)};
   display: flex;
   align-items: center;
   justify-content: center;
   margin-right: 15px;
+  z-index: 7;
 `;
 
 const MAC = styled.View`
@@ -44,7 +128,7 @@ const MAC = styled.View`
   align-items: center;
   justify-content: flex-start;
   border-radius: 30px;
-  margin-bottom: 15px;
+  position: relative;
 `;
 
 const MAW = styled.TouchableOpacity``;
--- src/constants/error.js
+++ src/constants/error.js
@@ -3,3 +3,5 @@ export const ERROR_ID_NOT_EXISTS = 'Такого id не існує!';
 export const ERROR_MY_ID = 'Ви ввели своє id!';
 export const ERROR_DIALOG_ALREADY_EXISTS =
   'Діалог з даним користувачем вже існує!';
+
+export const ERROR_AUDIO = 'Сталася помилка при відтворенні файлу!';
